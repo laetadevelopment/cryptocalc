33bb5e41d67499f916850fe1652cd01fa76fac7b
Mike Laeta <mike@laetadevelopment.com>
2022-04-30 12:50:25 -0600

    Revert "Development v0.0.1"

---
+++ sspwa/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {    
+    appleMobileWebAppCapable: 'yes',
+    iconPaths: {
+      favicon32: 'icons/favicon-32x32.png',
+      favicon16: 'icons/favicon-16x16.png',
+      appleTouchIcon: 'icons/ios/apple-icon-152.png',
+      maskIcon: 'icons/manifest-icon-512-masked.png',
+      msTileImage: 'icons/ms-icon-144x144.png'
+    }
+  },
+}
