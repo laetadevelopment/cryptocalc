e0d92d9e3ab0b081c6b5de6deaeaf92814211afb
Mike Laeta <mike@laetadevelopment.com>
2022-05-06 20:16:42 -0600

    Revert "Development v1.2.1"

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,18 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 require('dotenv').config()
 
-const kovankey = process.env.KOVAN_PRIVATE_KEY
-const kovanurl = process.env.KOVAN_RPC_URL
-const mainnetkey = process.env.MAINNET_PRIVATE_KEY
-const mainneturl = process.env.MAINNET_RPC_URL
+const key = process.env.PRIVATE_KEY
+const url = process.env.RPC_URL
 
 module.exports = {
   networks: {
     kovan: {
       provider: () => {
-        return new HDWalletProvider(kovankey, kovanurl)
+        return new HDWalletProvider(key, url)
       },
       network_id: '42',
       skipDryRun: true
     },
-    mainnet: {
-      provider: () => {
-        return new HDWalletProvider(mainnetkey, mainneturl)
-      },
-      network_id: '1'
-    }
   },
 
   compilers: {
